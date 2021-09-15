324278a873ce1584c3fc3d9a13f47baeebd0bd4f
Axel Mukwena <axel.muk@gmail.com>
2021-09-15 12:24:15 +0800

    Compression

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,26 @@ module.exports = {
             entry: 'src/main.js',
             title: 'Risksis'
         }
+    },
+    pluginOptions: {
+        compression: {
+            brotli: {
+                filename: '[file].br[query]',
+                algorithm: 'brotliCompress',
+                include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+                compressionOptions: {
+                    params: {
+                        [zlib.constants.BROTLI_PARAM_QUALITY]: 11
+                    }
+                },
+                minRatio: 0.8
+            },
+            gzip: {
+                filename: '[file].gz[query]',
+                algorithm: 'gzip',
+                include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+                minRatio: 0.8
+            }
+        }
     }
 }
